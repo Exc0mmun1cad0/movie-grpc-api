488c5e35eae45cfa7a73425a57e8b69de0528e48
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-03-21 00:01:29 +0300

    add main function for running app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"movie-service/internal/app"
+	"movie-service/internal/config"
+	"movie-service/pkg/postgres"
+	"movie-service/pkg/sl"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
-	
+	ctx := context.Background()
+
+	cfg := config.MustLoad()
+
+	log := app.SetupLogger(cfg.MovieService.Env)
+
+	log.Debug("example")
+
+	log.Info(
+		"starting app",
+		slog.String("env", cfg.MovieService.Env),
+		slog.String("address", fmt.Sprintf("localhost:%d", cfg.MovieService.GRPCPort)),
+	)
+
+	log.Info("initializing connection to postgres")
+	db, err := postgres.New(cfg.Postgres)
+	if err != nil {
+		log.Error("failed to connect to db(postgres)", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Info("connected to postgres")
+
+	log.Info("initializing app")
+	application := app.New(ctx, log, cfg.MovieService, db)
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	log.Info("started app")
+	go application.GRPCServer.MustRun()
+
+	<-stop
+	log.Info("stopping app")
+
+	application.GRPCServer.Stop()
+
+	postgres.MustClose(db)
+
+	log.Info("app stopped")
 }
